76c2c5b633cae38408f3aa5002f6dd300645dc7f
Lex Zavala <lex.zavala7@gmail.com>
2021-06-14 15:52:27 -0500

    completed exercise

---
+++ src/main/java/ExerciseMaven.java
@@ -0,0 +1,17 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class ExerciseMaven {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Something");
+        String userInput = scanner.nextLine();
+
+        System.out.println(StringUtils.isNumeric(userInput));
+        System.out.println(StringUtils.swapCase(userInput));
+        System.out.println(StringUtils.reverse(userInput));
+
+    }
+}
